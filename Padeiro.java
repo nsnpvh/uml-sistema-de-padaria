d4e8fd0aa06407ae3ddbca0ca66c80a46829fb63
Nelson <nelson.nascimento@gmail.com>
2025-03-23 20:26:27 -0400

    Classe Padeiro.java

---
+++ Padeiro.java
@@ -0,0 +1,14 @@
+public class Padeiro {
+
+    private String nome;
+    private int idPadeiro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
