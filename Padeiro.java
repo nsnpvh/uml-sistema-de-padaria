b0f0a3c9c4504f2217ae7f1fc505ec8bedd8300a
Nelson <nelson.nascimento@gmail.com>
2025-03-23 20:50:48 -0400

    classe Padeiro concluida

--- Padeiro.java
+++ Padeiro.java
@@ -2,13 +2,48 @@ public class Padeiro {
 
     private String nome;
     private int idPadeiro;
+    private Produto produto;
+    private Estoque estoque;
+
 
     public String getNome() {
         return nome;
+    }
+
+    public int getIdPadeiro() {
+        return idPadeiro;
+    }
+
+    public void setIdPadeiro(int idPadeiro) {
+        this.idPadeiro = idPadeiro;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-}
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Estoque getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Estoque estoque) {
+        this.estoque = estoque;
+    }
+
+    public void produzirProduto(Produto produto){
+
+    }
+
+    public void atualizarEstoque(Estoque estoque){
+
+    }
+
+ }
